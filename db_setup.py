a6593a68eb824977fbce0b9ed580345674e0d064
Tom Dong <verken487@gmail.com>
2024-10-04 00:51:04 +0100

    updated setup.db

--- db_setup.py
+++ db_setup.py
@@ -1,24 +1,34 @@
 import sqlite3
 
+PCPP_MSG_IDS = {  # Used to reference in other files
+    "table_name": "pcpp_message_ids",
+    "user_msg_id": "user_msg_id",
+    "bot_msg_id": "bot_msg_id",
+    "content_id": "content_id",
+}
+
 
 async def setup_db():
     async with sqlite3.connect("discord_db.db") as conn:
         cursor = conn.cursor()
     cursor.execute(  # Creates a parent table of message ids
         """
-        CREATE TABLE IF NOT EXISTS pcpp_msg_ids(
-            user_msg_id INT PRIMARY KEY,
-            bot_msg_id INT,
-            link_id INT,
+        CREATE TABLE IF NOT EXISTS ?(
+            ? INT,
+            ? INT,
+            ? INT UNIQUE,
             PRIMARY KEY (user_msg_id, bot_msg_id)
         )
-        """
+        """,
+        tuple(PCPP_MSG_IDS[key] for key in PCPP_MSG_IDS),
     )
 
     cursor.execute(
         """
-        CREATE TABLE IF NOT EXISTS links(
-            id
+        CREATE TABLE IF NOT EXISTS pcpp_contents(
+            id INT PRIMARY KEY,
+            pcpp VARCHAR(4000)
+            FOREIGN KEY id REFERENCES ?.
         )
         """
     )
